1b0100670bc786a3e52ae7c7ac449d2382710a45
Ivan A. Zenteno <k001.operator@gmail.com>
2010-04-19 19:58:12 -0500

    Adding source

---
+++ to_pdf.php
@@ -0,0 +1,7 @@
+<?php  if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+$to_pdf['path'] = BASE_PATH.'pdf';
+
+/* End of file to_pdf.php */
+/* Location: ./application/config/to_pdf.php */
+
