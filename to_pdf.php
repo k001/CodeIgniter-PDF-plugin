9f39554ececa6a8bcb7c74be24120609bd61eadc
Ivan A. Zenteno <k001.operator@gmail.com>
2010-04-19 20:35:44 -0500

    Established correct folders

--- to_pdf.php
+++
@@ -1,7 +0,0 @@
-<?php  if (!defined('BASEPATH')) exit('No direct script access allowed');
-
-$to_pdf['path'] = BASE_PATH.'pdf';
-
-/* End of file to_pdf.php */
-/* Location: ./application/config/to_pdf.php */
-
